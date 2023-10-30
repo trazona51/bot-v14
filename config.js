722a4aefc054965a41d7f984ab04fa7f2f2b7e09
trazona51 <134592856+trazona51@users.noreply.github.com>
2023-10-30 23:20:58 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+
+    token: "MTE2NjQxMDQ5ODczMzAwMjc2Mg.G3Jxpg.x3OHnfdYKEj7MHB3BU5OlnjamltVOIb1Duheww"
+}
---
+++ main.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js")
+const intents = new Discord.IntentsBitField(3276799)
+const bot = new Discord.Client({intents})
+const loadCommands = require("./Loaders/loadCommands")
+const loadEvents = require("./Loaders/loadEvents")
+const config = require("./config")
+ 
+bot.commands = new Discord.Collection()
+bot.color = "#ff2400";
+bot.function = {
+    createId: require("./Fonctions/createId"),
+    generateCaptcha: require("./Fonctions/generateCaptcha"),
+    searchSpam: require("./Fonctions/searchSpam")
+}
+
+bot.login(config.token)
+loadCommands(bot)
+loadEvents(bot)
